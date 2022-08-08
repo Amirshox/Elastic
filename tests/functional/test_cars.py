f6cef72e0eab23c2a752dd45a88463a8ab0a07a1
Amirshox Muxitdinov <amirshoxmuxitdinov@gmail.com>
2022-08-08 17:12:13 +0300

    tests

--- tests/functional/test_cars.py
+++ tests/functional/test_cars.py
@@ -8,3 +8,18 @@ def test_get_cars():
     with flask_app.test_client() as test_client:
         response = test_client.get('/api/v1/cars/')
         assert response.status_code == 200
+
+
+def test_post_car():
+    flask_app = create_app()
+
+    data = {
+        "state_number": "A777AA",
+        "region": "01",
+        "color": "red",
+        "brand": "lacetti"
+    }
+
+    with flask_app.test_client() as test_client:
+        response = test_client.post('/api/v1/cars/', data)
+        assert response.status_code == 201
